00c88b9f9356733b2c0c319c4d455c47bd3f3336
dsajdl01 <davidsajdl@yahoo.co.uk>
2015-12-24 22:20:11 +0000

    Adding colourController.js file. It creats a new controller named mainController. A controller manages the coulor app's data.

---
+++ angularJsUpRunning/ChapterSix/colourController.js
@@ -0,0 +1,13 @@
+app.controller('mainController', ['$http', function($http) {
+	var self = this;
+	self.text = 'getting data from http server'
+	self.items = [];
+	$http.get('https://raw.githubusercontent.com/dsajdl01/AngularJS_practice/master/angularJsUpRunning/ChapterSix/my-api-notes.json').then(function(response) {
+		console.log(response.data);
+		self.items = response.data;
+		}, function(errResponce){
+			console.log('error');
+			console.error('Error while fetching notes');
+		});
+}])
+				
