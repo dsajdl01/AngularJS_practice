940fcb399d5b7ec7bbc03ee2d434f72f76a38c10
dsajdl01 <davidsajdl@yahoo.co.uk>
2015-12-29 21:51:39 +0000

    Modified exampleController.js file. Coltroller of the app.

--- angularJsUpRunning/AngularJS-ext-excercise/js/controller/exampleController.js
+++ angularJsUpRunning/AngularJS-ext-excercise/js/controller/exampleController.js
@@ -4,20 +4,32 @@ myExampleApp.controller('exampleController', [ '$scope', '$modal', 'loadDataServ
   	var self = this;
   	self.showMe = "Hello David";
   	self.btnName = "Display data";
+  	self.process = false;
+  	self.showContent = false;
 
-/*	var myModal = $modal({scope: $scope, title: 'Processing Data...', content: 'Please wait', templateUrl:'js/views/template.html', show: false});
+	self.myModal = $modal({scope: $scope, title: 'Processing Data...', content: 'Please wait', templateUrl:'js/views/template.html', show: false});
 
 	self.showModal = function(){
 		console.log('show button is pressed');
-		myModal.$promise.then(myModal.show);
+		self.myModal.$promise.then(self.myModal.show);
 		self.btnProccess = false;
+		self.btnName = "Processing";
+		self.items = "";
+
+		loadDataService.loadHTMLColorCode(function(data){
+				console.log(data);
+				if(data == 400){
+					console.log(400, 'run time error occur.');
+				} 
+				else {
+					self.btnName = 'Done';
+					self.myModal.$promise.then(self.myModal.hide);
+					self.showContent = true;
+					self.items = data;
+					console.log(200, 'success');
+				}
+		});
 
 			
-	};
-
-	self.hideModel = function(){
-		console.log('Hide button is pressed');
-		myModal.$promise.then(myModal.hide);
-	};*/
-  	 	
+	};	
 }]);
