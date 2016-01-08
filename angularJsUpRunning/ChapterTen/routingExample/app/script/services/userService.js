5d7774b0a2059ed2262d83d73aa4e5520463a039
dsajdl01 <davidsajdl@yahoo.co.uk>
2016-01-08 19:45:41 +0000

    Adding userService.js file. Service for fifa app to provide login.

---
+++ angularJsUpRunning/ChapterTen/routingExample/app/script/services/userService.js
@@ -0,0 +1,21 @@
+fifaApp.factory('UserService',[ '$http', function($http){
+
+ 		var service = {
+ 			isLoggedIn: false,
+
+ 			session: function() {
+ 				return $http.get('/api/session').then(function(response){
+ 					service.isLoggedIn = true;
+ 					return response;
+ 				});
+ 			},
+
+ 			login: function(user){
+ 				return $http.post('/api/login', user).then(function(response){
+ 					service.isLoggedIn = true;
+ 					return response;
+ 				});
+ 			}
+ 		};
+ 		return service;
+}]);
