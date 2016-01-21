962ff8e051fd9993d0b299505af37d385f84adf6
dsajdl01 <davidsajdl@yahoo.co.uk>
2016-01-21 20:00:46 +0000

    adding directive.js file Ditectie for app

---
+++ angularJsUpRunning/ChapterEleven/directiveBrokenReference/directive.js
@@ -0,0 +1,34 @@
+stockBrRefApp.directive('stockWidget', [function(){
+		// Runs during compile
+		return {
+			// name: '',
+			// priority: 1,
+			// terminal: true,
+			scope: {
+				stockData: '='
+			}, // {} = isolate, true = child, false/undefined = no change
+			// controller: function($scope, $element, $attrs, $transclude) {},
+			// require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
+		 	restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
+			// template: '',
+			templateUrl: 'stock.html',
+			// replace: true,
+			// transclude: true,
+			// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
+			link: function($scope, $element, $attrs) {
+
+				$scope.getChange = function(stock){
+					return Math.ceil(((stock.price - stock.previous) / 
+										stock.previous) * 100);
+					};
+
+				$scope.changeStock = function(){
+					$scope.stockData = {
+						name: 'Directive Stock',
+						price: 500,
+						previous: 200
+					};
+				};
+			}
+		};
+}]);
