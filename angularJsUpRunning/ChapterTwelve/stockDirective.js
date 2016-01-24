1b24ea667970d68757f0c91ca78d1500fe33135f
dsajdl01 <davidsajdl@yahoo.co.uk>
2016-01-24 19:15:47 +0000

    Adding stockDirective.js file. Directive foe stock matker. From Chapter Eleven - directiveScopeAdvanced folder

---
+++ angularJsUpRunning/ChapterTwelve/stockDirective.js
@@ -0,0 +1,36 @@
+angular.module('advanceDivScopeApp',[])
+	 .directive('stockWidget', [ function(){
+		// Runs during compile
+		return {
+			// name: '',
+			// priority: 1,
+			// terminal: true,
+			scope: {
+				stockData: '=',
+				stockTitle: '@',
+				whenSelect: '&'
+			}, // {} = isolate, true = child, false/undefined = no change
+			// controller: function($scope, $element, $attrs, $transclude) {},
+			// require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
+			// restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
+			// template: '',
+		 	templateUrl: 'stock.html',
+			// replace: true,
+			// transclude: true,
+			// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
+			link: function($scope, $element, $attrs) {
+				$scope.getChange = function(stock){
+					return Math.ceil(((stock.price - stock.previous) / 
+										stock.previous) * 100);
+					};
+
+				$scope.onSelect = function() {
+					$scope.whenSelect({
+						stockName: $scope.stockData.name,
+						stockPrice: $scope.stockData.price,
+						stockPrevious: $scope.stockData.previous
+					});
+				};
+			}
+		};
+	}]);
