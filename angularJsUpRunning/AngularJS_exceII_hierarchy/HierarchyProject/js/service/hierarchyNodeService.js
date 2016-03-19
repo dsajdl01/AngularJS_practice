748b6739ca5d01eab7912a00d8aef788de3723b9
dsajdl01 <davidsajdl@yahoo.co.uk>
2016-03-19 22:26:59 +0000

    Adding hierarchyNodeService.js file to load JSON node data

---
+++ angularJsUpRunning/AngularJS_exceII_hierarchy/HierarchyProject/js/service/hierarchyNodeService.js
@@ -0,0 +1,29 @@
+myMngtHierarchyApp.factory('hierarchyNodeService',
+							['$resource', '$http',function($resource, $http){
+	return new hierarchyNodeService($resource, $http);
+}]);
+
+function hierarchyNodeService($resource, $http){
+	this.$resource = $resource;
+//	this.$http = $http;
+	var self = this;
+	
+	self.getSelectedNode = function(success, fail){
+		console.log("in========");
+		var URL = 'https://raw.githubusercontent.com/dsajdl01/AngularJS_practice/master/angularJsUpRunning/AngularJS_exceII_hierarchy/dataNodeHierarchy.json';
+		
+		return $http(
+						{
+							method: 'GET',
+							url: URL
+						})
+					.then(function successCallBack(topRoot){
+						console.log("in top Root = ", topRoot);
+						success(topRoot.data);
+					},
+					function errorCallBack(error){
+						fail(error);
+					});
+	};
+
+}
