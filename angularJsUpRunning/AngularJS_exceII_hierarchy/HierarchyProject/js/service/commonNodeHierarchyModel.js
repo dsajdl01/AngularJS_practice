e3c87eef1512c17d0c869bfaa540bd552d0f167f
dsajdl01 <davidsajdl@yahoo.co.uk>
2016-03-19 22:28:15 +0000

    Adding commonNodeHierarchyModel.js file. The common data to be shared throughtout the hierarchy application

---
+++ angularJsUpRunning/AngularJS_exceII_hierarchy/HierarchyProject/js/service/commonNodeHierarchyModel.js
@@ -0,0 +1,8 @@
+myMngtHierarchyApp.factory('commonNodeHeirarchyModel',[ function(){
+	return new commonNodeHeirarchyModel();
+}]);
+
+function commonNodeHeirarchyModel(){
+	var self = this;
+	self.rootNode = {};
+}
