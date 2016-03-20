bc8b62f87cd4f221fb8d0cc158a8b777a1351fad
dsajdl01 <davidsajdl@yahoo.co.uk>
2016-03-20 21:38:23 +0000

    Adding modalDialogBoxService.js file - Service to display html templates

---
+++ angularJsUpRunning/AngularJS_exceII_hierarchy/HierarchyProject/js/service/modalDialogBoxService.js
@@ -0,0 +1,34 @@
+myMngtHierarchyApp.factory('modalDialogBoxService', ['$modal', function($modal){
+	return new modalDialogBoxService($modal);
+}]);
+
+function modalDialogBoxService($modal){
+	 var self = this;
+
+	 self.modalData = {
+	 	show: false
+	 };
+
+	 self.setTemplate = function(template){
+	 	self.modalData.templateUrl = template;
+	 };
+
+	 self.showDialog = function(){
+	 	if(!self.modalData.templateUrl){
+	 		throw "No template set for modal dialog";
+	 	}
+
+	 	self.modalInstance = $modal(self.modalData);
+
+	 	self.modalInstance.$promise.then(self.modalInstance.show);
+	 };
+
+	 self.hideDialog = function(){
+	 	self.modalInstance.hide();
+	 }
+
+	 self.notifyAndHide = function(object){
+	 	self.notify(object);
+	 	self.hideDialog();
+	 };
+}
