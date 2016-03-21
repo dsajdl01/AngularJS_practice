cc24c594b74734aba9f0bc97998883c085c0c219
dsajdl01 <davidsajdl@yahoo.co.uk>
2016-03-21 22:29:43 +0000

    modified mngtHierarchyController.js file by finished getPathToNode() function and removing certain variables

--- angularJsUpRunning/AngularJS_exceII_hierarchy/HierarchyProject/js/controller/mngtHierarchyController.js
+++ angularJsUpRunning/AngularJS_exceII_hierarchy/HierarchyProject/js/controller/mngtHierarchyController.js
@@ -9,58 +9,31 @@ myMngtHierarchyApp.controller( 'mngtHierarchyController', ['mngtHierarchyNodeSer
 	self.showPage = false;
 	self.commonNodeHeirarchyModel = commonNodeHeirarchyModel;
 	self.test = 'any content';
-	self.nodes = { 
-		repeatSelect: null,
-		availableOptions:[
-								 	{
-											id: 1,
-											name: 'First note',
-											done: false,	
-											someRandom: 31431
-								      },
-									  {
-											id: 2,
-											name: 'Second note',
-											done: false
-									  },
-								  	  {
-											id: 3,
-											name: 'Finishes Third Note',
-											done: true
-									   } ]
-						};
+
 	self.init = function(){
 		var isAssumeIdentity = false;
 		var isNodeLoaded = false;
+		var allPath = [];
 		mngtHierarchyNodeServiceProvider.loadTopNode(function(responce){
 			isAssumeIdentity = responce;
 			console.log(isAssumeIdentity, commonNodeHeirarchyModel.rootNode[0].name);
-			var pptt = getPathToNodes(commonNodeHeirarchyModel.rootNode[0]);
-			console.log("gdggdgd: ",pptt);
+			self.nodes  = getPathToNodes(commonNodeHeirarchyModel.rootNode[0], "", allPath);
+			self.nodes.unshift("[Assume Identity]");
+			console.log( self.selectedNode, "gdggdgd: ", self.nodes);
+			mngtHierarchyNodeServiceProvider.displayAssumeDialogBox(self.nodes);
 		});
-		// mngtHierarchyNodeServiceProvider.displayAssumeDialogBox();
+		
+
 	};
 
-	var getPathToNodes = function(nodes){
+	var getPathToNodes = function(nodes, pathToParent, allPath){
 
-		namePath += nodes.name+"/";
-
+		var pathToCurrentNode = pathToParent + (pathToParent.length == 0 ? "": ">") + nodes.name
+		allPath.push(pathToCurrentNode);
 		for(var i = 0; i < nodes.child.length; i++){
-			//console.log(nodes.child[i]);
-/*			if(i == 0){
-				namePath +="XXX";
-			}*/
-			console.log(">>>>-",nodes.child[i].name , (!nodes.child[i].child[0]));
-			if(nodes.child[i].child[0]){
-				namePath += "false/";
-			} else {
-				namePath += "true/";
-			}
-			//namePath += "/" + nodes.child[i].name + "_" +i + " "; 
-			//path[i] = topName + "/" + nodes.child[i].name;
-			getPathToNodes(nodes.child[i]); 
+			getPathToNodes(nodes.child[i], pathToCurrentNode, allPath);
 		}
-		return namePath;
+		return allPath;
 	}
 	
 }]);
