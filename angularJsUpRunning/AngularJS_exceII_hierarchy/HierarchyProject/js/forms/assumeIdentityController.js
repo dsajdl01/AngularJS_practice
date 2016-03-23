225bb2b2fa2b1cd23d60c0d288d2c3df3b21526a
dsajdl01 <davidsajdl@yahoo.co.uk>
2016-03-23 22:36:41 +0000

    modified assumeIdentity template and controller

--- angularJsUpRunning/AngularJS_exceII_hierarchy/HierarchyProject/js/forms/assumeIdentityController.js
+++ angularJsUpRunning/AngularJS_exceII_hierarchy/HierarchyProject/js/forms/assumeIdentityController.js
@@ -1,9 +1,24 @@
+'use strict';
 myMngtHierarchyApp.controller('assumeIdentityController',['modalDialogBoxService',
 	function(modalDialogBoxService) {
 	
 	var self = this;
+	self.showMessage = false;
 	self.allPath = modalDialogBoxService.shareModalData.pathToEachNode;
 	self.selectedNode = self.allPath[0];
-	console.log(self.allPath, " in assumeIdentityController!!!");
+
+	self.getSelectedValue = function(selectedPath){
+		
+		if(selectedPath !== "[Assume Identity]"){
+			self.showMessage = false;
+			modalDialogBoxService.notifyAndHide(selectedPath);
+		} else {
+			self.showMessage = true;
+		}
+	};
+
+	self.cancel = function () {
+		modalDialogBoxService.hideDialog();
+	}
 
 }]);
