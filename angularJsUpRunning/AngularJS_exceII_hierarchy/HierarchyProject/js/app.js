5a7d0aeacb2f4eec01a028e41ca8b6ea8d83e261
dsajdl01 <davidsajdl@yahoo.co.uk>
2016-03-25 21:47:53 +0000

    modified app.js file by adding dependencies ngRoute and config

--- angularJsUpRunning/AngularJS_exceII_hierarchy/HierarchyProject/js/app.js
+++ angularJsUpRunning/AngularJS_exceII_hierarchy/HierarchyProject/js/app.js
@@ -1,5 +1,20 @@
 var myMngtHierarchyApp = angular.module('myMngtHierarchyApp', [
+	'ngRoute',
 	'ngResource',
 	'ngMessages',
 	'mgcrea.ngStrap'
 	]);
+
+myMngtHierarchyApp.config(['$routeProvider', function($routeProvider) {
+	  $routeProvider
+
+      // Add more views here for routing
+	  .when('/homeViewAccount', {
+		  templateUrl: 'js/views/selectedNodeInfo.html'
+	  })
+	  .when('/viewAccountInfo',{
+	  	templateUrl: 'js/views/accountManagementHierarchyInfo.html'
+	  })
+	  .otherwise({redirectTo: '/homeViewAccount'})
+	}]
+);
