debcfcdf0a23bd17b8e3e1f274516cdae18742aa
dsajdl01 <davidsajdl@yahoo.co.uk>
2016-04-03 20:56:19 +0100

    modified nodeController.js file by adding few methods and re-orginise code

--- angularJsUpRunning/AngularJS_exceII_hierarchy/HierarchyProject/js/controller/nodeController.js
+++ angularJsUpRunning/AngularJS_exceII_hierarchy/HierarchyProject/js/controller/nodeController.js
@@ -8,40 +8,46 @@ myMngtHierarchyApp.controller('nodeController',[ 'commonNodeHeirarchyModel','mng
 
 			self.init = function(){
 				self.assumedNode = self.commonNodeHeirarchyModel.selectedTopNode;
-				userSelectedNodeDetails = getNodesDetails(self.commonNodeHeirarchyModel.userSelectedNode.id);
-				self.titleOfNode = self.assumedNode.name;
-				self.numberOfChild = getChildMessage(self.commonNodeHeirarchyModel.userSelectedNode);
-				self.profesionInfo = getProfesionMessage(self.commonNodeHeirarchyModel.userSelectedNode, userSelectedNodeDetails);
-				self.workSince = getWorkMessage(self.commonNodeHeirarchyModel.userSelectedNode, userSelectedNodeDetails);
-				self.commensData = userSelectedNodeDetails.comments;
-				self.detailsTitle = self.commonNodeHeirarchyModel.userSelectedNode.name + " comments:";	
+				initializeViewVariables(self.assumedNode, getNodesDetails(self.commonNodeHeirarchyModel.userSelectedNode.id))
+			}
+
+			self.validateNewNodeName = function(newNodeName){
+				console.log("validateNewNodeName: ", newNodeName);
+				return true;
+			}
+			self.updateSelectedNodeName = function(newName){
+				console.log("updateSelectedNodeName: ", newName);
 			}
 
 			self.userSelectedNode = function(node){
 				self.commonNodeHeirarchyModel.userSelectedNode = node;
-				userSelectedNodeDetails = getNodesDetails(node.id);
+				initializeViewVariables(node, getNodesDetails(node.id));
+			};		
+			
+			var initializeViewVariables = function(node, details){
+				console.log("aditable: ", self.assumedNode.name, " <and> ", self.assumedNode.id);
 				self.titleOfNode = node.name;
 				self.numberOfChild = getChildMessage(node);
-				self.profesionInfo = getProfesionMessage(node, userSelectedNodeDetails);
-				self.workSince = getWorkMessage(node, userSelectedNodeDetails);
-				self.commensData = userSelectedNodeDetails.comments; 
+				self.profesionInfo = getProfesionMessage(node, details);
+				self.workSince = getWorkMessage(node, details);
+				self.commensData = details.comments; 
 				self.detailsTitle = node.name + " comments:";
-			};		
+			}
 
 			var getNodesDetails = function(nodeId){
-	//			console.log(nodeId);
 				return mngtHierarchyNodeServiceProvider.getSelectedNodeDetails(nodeId);
-			}	
-			
+			}
+
 			var getChildMessage = function(child){
 				if(!child.child.length){
-					return child.name + " does not have any child."
+					return child.name + " does not have any child.";
 				} else if (child.child.length == 1 ) {
-					return child.name + " has 1 child."
+					return child.name + " has 1 child.";
 				} else {
-					return child.name + " has " + child.child.length + " childern."
+					return child.name + " has " + child.child.length + " childern.";
 				}
 			}
+
 			var getWorkMessage = function(child, details){
 				return child.name + " has been working for our company since " + details.start;
 			}
