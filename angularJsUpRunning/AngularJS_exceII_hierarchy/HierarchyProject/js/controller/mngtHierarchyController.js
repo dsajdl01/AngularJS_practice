3953684da8fa14c6ead9a781becff66ff4391dcc
dsajdl01 <davidsajdl@yahoo.co.uk>
2016-04-15 20:24:41 +0100

    modified mngtHierarchyController.js by cleaning code

--- angularJsUpRunning/AngularJS_exceII_hierarchy/HierarchyProject/js/controller/mngtHierarchyController.js
+++ angularJsUpRunning/AngularJS_exceII_hierarchy/HierarchyProject/js/controller/mngtHierarchyController.js
@@ -4,76 +4,82 @@ myMngtHierarchyApp.controller( 'mngtHierarchyController', ['mngtHierarchyNodeSer
 	var self = this;
 	var isNodeLoaded = false;
 	self.showPage = false;
-	self.isDisabled = false;
+	self.isTopNavigationBtnDisabled = false;
 	self.commonNodeHeirarchyModel = commonNodeHeirarchyModel;
-	self.commonNodeHeirarchyModel.allNodesDetails = [];
-	self.accountTitle = "";
+	self.accountTitle;
 	self.data = new Date();
 
-	self.init = function(){
+	self.init = function()
+	{
 		var isAssumeIdentity = false;
 		isNodeLoaded = false;
 		self.showPage = false;
-		self.isDisabled = false;
-		console.log("self.isDisabled: ", self.isDisabled);
-		$location.path('/homeViewAccount');
+		self.isTopNavigationBtnDisabled = false;
+		relocatePageToHomePage();
 		mngtHierarchyNodeServiceProvider.loadTopNode(function(responce){
 			isNodeLoaded = responce;
-			mngtHierarchyNodeServiceProvider.loadNodeDetails(function(responce){
-			});
-			if(isNodeLoaded){
+			mngtHierarchyNodeServiceProvider.loadNodeDetails(function(responce){});
+			if(isNodeLoaded)
+			{
 				self.getAssumeIdentityDialogBox();
-			} else {
+			} else 
+			{
 		 		toaster.pop("error","Error occer while app was downloading data.");
-
 			}
 		});
 	};
 
-	self.getAssumeIdentityDialogBox = function(){
-		var allPath = [];
-		var nodes  = getAllPathToNodes(commonNodeHeirarchyModel.rootNode[0], "", []);
+	self.getAssumeIdentityDialogBox = function()
+	{	
+		var nodes  = getAllPathToEachNode(commonNodeHeirarchyModel.rootNode[0], "", []);
 			nodes.unshift("[Assume Identity]");
-			mngtHierarchyNodeServiceProvider.displayAssumeDialogBox(nodes, function(selectedNodeName){
-				if(selectedNodeName == false){
-					self.isDisabled = selectedNodeName;
-					console.log("self.isDisabled in responce: ", self.isDisabled);
+			mngtHierarchyNodeServiceProvider.displayAssumeDialogBox(nodes, function(selectedNodeName)
+			{
+				if(selectedNodeName == false)
+				{
+					self.isTopNavigationBtnDisabled = selectedNodeName;
 					self.accountTitle = "";
-					$location.path('/templateAssumeIdentity');
-				} else {
-					self.isDisabled = true;
-					console.log("self.isDisabled in responce: ", self.isDisabled);
+				} else
+				{
+					self.isTopNavigationBtnDisabled = true;
 					self.accountTitle = "Profile of " +selectedNodeName;
 				}
 				isAssumeIdentity = true;
 				canPageBeDisplayed(isNodeLoaded, isAssumeIdentity);
 			});
-	}
+	};
 
-	self.loadPage = function(){
+	self.loadPage = function()
+	{
 		self.showPage = false;
-		$location.path('/homeViewAccount');
 		self.getAssumeIdentityDialogBox();
-	}
+	};
 
-	var getAllPathToNodes = function(nodes, pathToParent, allPath){
+	self.displayAboutDialog = function() 
+	{
+		mngtHierarchyNodeServiceProvider.displayAboutDialogBox();
+	};
 
+	var getAllPathToEachNode = function(nodes, pathToParent, allPath)
+	{
 		var pathToCurrentNode = pathToParent + (pathToParent.length == 0 ? "": ">") + nodes.name
 		allPath.push(pathToCurrentNode);
 		nodes.pathToNode = pathToCurrentNode;
 		self.commonNodeHeirarchyModel.allNodesDetails.push(nodes); 
-		for(var i = 0; i < nodes.child.length; i++){
-			getAllPathToNodes(nodes.child[i], pathToCurrentNode, allPath);
+		for(var i = 0; i < nodes.child.length; i++)
+		{
+			getAllPathToEachNode(nodes.child[i], pathToCurrentNode, allPath);
 		}
 		return allPath;
 	};
 
-	var canPageBeDisplayed = function(isNodeLoaded, isAssumeIdentity){
+	var canPageBeDisplayed = function(isNodeLoaded, isAssumeIdentity)
+	{
 		self.showPage = isNodeLoaded && isAssumeIdentity;
 	};
-	
-	self.displayAboutDialog = function() {
-		mngtHierarchyNodeServiceProvider.displayAboutDialogBox();
-	};
 
+	var relocatePageToHomePage = function()
+	{
+		$location.path('/homeViewAccount');
+	};
 }]);
