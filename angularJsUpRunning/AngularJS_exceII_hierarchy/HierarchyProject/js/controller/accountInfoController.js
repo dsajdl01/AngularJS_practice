a30b85283c4c707f2669a5c202bec591a48a9e69
dsajdl01 <davidsajdl@yahoo.co.uk>
2016-04-21 20:18:26 +0100

    modified accountInfoController.js file by re-orgenizing code - cleaning code

--- angularJsUpRunning/AngularJS_exceII_hierarchy/HierarchyProject/js/controller/accountInfoController.js
+++ angularJsUpRunning/AngularJS_exceII_hierarchy/HierarchyProject/js/controller/accountInfoController.js
@@ -1,96 +1,37 @@
-myMngtHierarchyApp.controller('accountInfoController',[ '$scope', 'commonNodeHeirarchyModel', 'mngtHierarchyNodeServiceProvider',
-				 function($scope, commonNodeHeirarchyModel, mngtHierarchyNodeServiceProvider) {
+myMngtHierarchyApp.controller('accountInfoController',[ '$scope', 'calculateTimeService', 'commonNodeHeirarchyModel', 'mngtHierarchyNodeServiceProvider',
+				 function($scope, calculateTimeService, commonNodeHeirarchyModel, mngtHierarchyNodeServiceProvider) {
 	
 	var self = this;
-	var commonNodeHeirarchyModel = commonNodeHeirarchyModel;
-	var selectedNodeId = 0;
-	var selectedNodeObject = "";
-	self.possition = "";
-	self.dob = "";
-	self.startWorkingDay = "";
-	self.comments = ""
+
+	self.possition = null;
+	self.dob = null;
+	self.startWorkingDay = null;
+	self.comments = null;
+
 	$scope.$watch(
 		 function()
             {
-				selectedNodeId = commonNodeHeirarchyModel.selectedTopNode.id;
-				if(selectedNodeId){
-					selectedNodeObject = mngtHierarchyNodeServiceProvider.getSelectedNodeDetails(selectedNodeId);
+				if(getSelectedNodeId())
+				{
+					selectedNodeObject = mngtHierarchyNodeServiceProvider.getSelectedNodeDetails(getSelectedNodeId());
 					initializeVariables(selectedNodeObject);
 				}
 			}
 	);
 	
-	var initializeVariables = function(selectedNodeObject) {
+	var initializeVariables = function(selectedNodeObject)
+	{
 		self.possition = selectedNodeObject.possition;
 		self.dob = selectedNodeObject.dob;
-		self.age = calculateDiffYearTocurrentDayFromFormatDay(self.dob)
+		self.age = calculateTimeService.calculateDiffYearToCurrentDayFromFormatDay(self.dob);
 		self.startWorkingDay = selectedNodeObject.start;
 		self.comments = selectedNodeObject.comments;
-		self.yearsWorking = calculateDiffYearTocurrentDayFromFormatDay(self.startWorkingDay);
-		self.daysWorking = calculateDiffDaysTocurrentDayFromFormatDayAndYear(self.startWorkingDay, self.yearsWorking);
+		self.yearsWorking = calculateTimeService.calculateDiffYearToCurrentDayFromFormatDay(self.startWorkingDay);
+		self.daysWorking = calculateTimeService.calculateDiffDaysToCurrentDayFromFormatDayAndYear(self.startWorkingDay, self.yearsWorking);
 	}
 
-	var calculateDiffDaysTocurrentDayFromFormatDayAndYear = function(format, yearsWorking){
-		var day   = parseInt(format.substring(0,2));
-    	var month  = parseInt(format.substring(3,5));
-   	 	var year   = parseInt(format.substring(6,10));
-   	 	var dayToCurerntDay = getDiffDayToCurrentDay(new Date(year, month-1, day));
-   	 	var yearsDay = getDayOfTheYear(year, new Date().getFullYear(), yearsWorking);
-   	 	var diffDay = dayToCurerntDay - yearsDay;
-    	return diffDay;
-	}
-	var getDiffDayToCurrentDay = function(startDay){
-		var timeDiff = Math.abs(new Date().getTime() - startDay.getTime());   
-    	var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
-    	return diffDays;
-	}
-	var getDayOfTheYear = function(startYear, currentYear, yearsWorking) {
-		if(startYear > currentYear){
-			throw "Start year cannot be greater that current year!";
-		}
-		var leapYearDays = 0;
-		for(var i = startYear; i <= currentYear; i++){
-			if(leapYear(i)){
-				leapYearDays++;
-			}
-		}
-		return (yearsWorking * 365) + leapYearDays;
-	}
-
-
-	leapYear = function(year)
+	var getSelectedNodeId = function()
 	{
-  		return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
-	}
-
-
-	var calculateDiffYearTocurrentDayFromFormatDay = function(format) {
-    	var day   = parseInt(format.substring(0,2));
-    	var month  = parseInt(format.substring(3,5));
-   	 	var year   = parseInt(format.substring(6,10));
-   	 	var diffYear = calculateDifferentYearTocurrentDay(month, day, year);
-    	return diffYear;
-	}
-
-	var calculateDifferentYearTocurrentDay = function (Month, Day, Year){
-		var todayDate = new Date();
-	   	var todayYear = todayDate.getFullYear();
-	  	var todayMonth = todayDate.getMonth();
-	  	var todayDay = todayDate.getDate();
-	  	var age = todayYear - Year;
-
-		if (todayMonth < Month - 1){
-		    age--;
-		}
-
-		if (Month - 1 == todayMonth && todayDay < Day){
-		    age--;
-		}
-		return age;
-	}
-
-	var getSelectedNodeID = function(){
 		return commonNodeHeirarchyModel.selectedTopNode.id;
 	};
-	
 }]);
