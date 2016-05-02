e20d3746288f92cecee5ff6de297c2fb445d2d17
dsajdl01 <davidsajdl@yahoo.co.uk>
2016-05-02 16:01:30 +0100

    modified mngtHierarchyNodeServiceProvider.js file by cleaning code

--- angularJsUpRunning/AngularJS_exceII_hierarchy/HierarchyProject/js/service/mngtHierarchyNodeServiceProvider.js
+++ angularJsUpRunning/AngularJS_exceII_hierarchy/HierarchyProject/js/service/mngtHierarchyNodeServiceProvider.js
@@ -4,36 +4,34 @@ myMngtHierarchyApp.factory('mngtHierarchyNodeServiceProvider',
 		return new mngtHierarchyNodeServiceProvider(hierarchyNodeService, commonNodeHeirarchyModel, modalDialogBoxService);
 }]);
 
-function mngtHierarchyNodeServiceProvider(hierarchyNodeService, commonNodeHeirarchyModel, modalDialogBoxService){
+function mngtHierarchyNodeServiceProvider(hierarchyNodeService, commonNodeHeirarchyModel, modalDialogBoxService) {
 		var self = this;
 	
 		self.loadTopNode = function(callBack)
 		{
 			hierarchyNodeService.getSelectedNode(
-			 	function(rootNode){
+			 	function(rootNode) {
 			 		commonNodeHeirarchyModel.rootNode = rootNode;
 			 		callBack(true);
 			 	},
 			 	function(fail){
 			 		callBack(false);
 			 	}
-
 			);
 		};
 		
 		self.loadNodeDetails = function(callback)
 		{
 			hierarchyNodeService.getNodesDetails(
-				function(nodesDetails){
+				function(nodesDetails) {
 			 		commonNodeHeirarchyModel.nodesDetails = nodesDetails;
 			 		callback(true);
 			 	},
-			 	function(fail){
+			 	function(fail) {
 			 		callback(false);
 			 	}
-
 			);
-		}
+		};
 
 		self.displayAssumeDialogBox = function( path, callBack )
 		{
@@ -44,8 +42,8 @@ function mngtHierarchyNodeServiceProvider(hierarchyNodeService, commonNodeHeirar
 
 			modalDialogBoxService.notify = function(selectedPath)
 			{
-				console.log("fff", selectedPath);
-				if(!selectedPath){
+				if(!selectedPath)
+				{
 					callBack(selectedPath);
 				}
 				else
@@ -65,11 +63,12 @@ function mngtHierarchyNodeServiceProvider(hierarchyNodeService, commonNodeHeirar
 
 		self.getSelectedNodeDetails = function(selectedNodeId)
 		{
-			for(var i = 0; i < commonNodeHeirarchyModel.nodesDetails.length; i++){
-				if(commonNodeHeirarchyModel.nodesDetails[i].id == selectedNodeId){
+			for(var i = 0; i < commonNodeHeirarchyModel.nodesDetails.length; i++)
+			{
+				if(commonNodeHeirarchyModel.nodesDetails[i].id == selectedNodeId) {
 					return commonNodeHeirarchyModel.nodesDetails[i];
-				}
-			}
+				};
+			};
 			return null;
 		};
 
@@ -77,19 +76,19 @@ function mngtHierarchyNodeServiceProvider(hierarchyNodeService, commonNodeHeirar
 		{
 			var nodeName = selectedPath.split(">");
 			return nodeName[nodeName.length - 1];
-		}
-
+		};
 
 		var saveSelectedNode = function(selectedPath)
 		{
-			for(var i = 0; i < commonNodeHeirarchyModel.allNodesDetails.length; i++){
-				if(commonNodeHeirarchyModel.allNodesDetails[i].pathToNode == selectedPath){
+			for(var i = 0; i < commonNodeHeirarchyModel.allNodesDetails.length; i++)
+			{
+				if(commonNodeHeirarchyModel.allNodesDetails[i].pathToNode == selectedPath) {
 					commonNodeHeirarchyModel.selectedTopNode = commonNodeHeirarchyModel.allNodesDetails[i];
 					commonNodeHeirarchyModel.selectedTopNode.isRootNode = true;
 					commonNodeHeirarchyModel.userSelectedNode = commonNodeHeirarchyModel.allNodesDetails[i];
 					break;
-				}
-			}
+				};
+			};
 		};
-}
+};
 	
