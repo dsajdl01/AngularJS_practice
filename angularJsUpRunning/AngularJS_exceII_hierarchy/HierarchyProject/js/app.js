3abfa43596fff30ea4ca31180a47fcbd9760895f
dsajdl01 <davidsajdl@yahoo.co.uk>
2016-05-16 23:13:48 +0100

    modified app.js by adding to routerProvider permissions

--- angularJsUpRunning/AngularJS_exceII_hierarchy/HierarchyProject/js/app.js
+++ angularJsUpRunning/AngularJS_exceII_hierarchy/HierarchyProject/js/app.js
@@ -6,18 +6,44 @@ var myMngtHierarchyApp = angular.module('myMngtHierarchyApp', [
 	'mgcrea.ngStrap'
 	]);
 
+myMngtHierarchyApp.run(['$rootScope', '$location', 'Auth', function ($rootScope, $location, Auth) {
+
+    $rootScope.$on('$routeChangeStart', function (event, next) {
+    	if(!Auth.checkIfAssumedIdentity(next)){
+    		event.preventDefault();
+            $location.path("/templateAssumeIdentity");
+    	} 
+    	else if (!Auth.checkPermissionForView(next)){
+            event.preventDefault();
+            $location.path("/homeViewAccount");
+        }
+    });
+  }]);
+
 myMngtHierarchyApp.config(['$routeProvider', function($routeProvider) {
 	  $routeProvider
 
       // Add more views here for routing
 	  .when('/homeViewAccount', {
-		  templateUrl: 'js/views/selectedNodeInfo.html'
+		  templateUrl: 'js/views/selectedNodeInfo.html',
+		  requireAssumedIdentity: false,
+		  requiresAuthentication: false
 	  })
 	  .when('/viewAccountInfo',{
-	  	templateUrl: 'js/views/accountManagementHierarchyInfo.html'
+	  	templateUrl: 'js/views/accountManagementHierarchyInfo.html',
+	  	requireAssumedIdentity: true,
+	  	requiresAuthentication: true,
+  	 	permissions: ["admin", "viewer"]
 	  })
 	  .when('/templateAssumeIdentity',{
-	  	templateUrl: 'js/views/AssumeIdentityTemplate.html'
+	  	templateUrl: 'js/views/AssumeIdentityTemplate.html',
+	  	requireAssumedIdentity: false,
+	  	requiresAuthentication: false
+	  })
+	  .when('/myProfile',{
+	  	templateUrl: 'js/views/myProfile.html',
+	  	requireAssumedIdentity: true,
+	  	requiresAuthentication: false
 	  })
 	  .otherwise({redirectTo: '/homeViewAccount'})
 	}]
